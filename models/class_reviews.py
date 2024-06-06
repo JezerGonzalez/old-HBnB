229b4679d04bd293ea1c8f40e8b55622152ca117
Gadiel02 <gadielrivera02@gmail.com>
2024-06-06 12:50:52 -0400

    adding an extra underscore to all self

--- models/class_reviews.py
+++ models/class_reviews.py
@@ -3,38 +3,38 @@ from datetime import datetime
 
 class Review:
     def __init__(self, user_name):
-        self._id = str(uuid.uuid4())  # Genera un ID único
-        self._user_name = user_name
-        self._review_text = ""
-        self._created = datetime.now()
-        self._updated = datetime.now()
+        self.__id = str(uuid.uuid4())  # Genera un ID único
+        self.__user_name = user_name
+        self.__review_text = ""
+        self.__created = datetime.now()
+        self.__updated = datetime.now()
 
     # Getters
     @property
     def id(self):
-        return self._id
+        return self.__id
 
     @property
     def user_name(self):
-        return self._user_name
+        return self.__user_name
 
     @property
     def review_text(self):
-        return self._review_text
+        return self.__review_text
 
     @property
     def created(self):
-        return self._created
+        return self.__created
 
     @property
     def updated(self):
-        return self._updated
+        return self.__updated
 
     # Setters
     @user_name.setter
     def user_name(self, value):
-        self._user_name = value
-        self._updated = datetime.now()
+        self.__user_name = value
+        self.__updated = datetime.now()
 
     @review_text.setter
     def review_text(self, value):
