d36942c157ffd7861d6cd2765a2b79bd18ab22cf
Gadiel02 <gadielrivera02@gmail.com>
2024-06-06 13:01:53 -0400

    add file class contry to branch

---
+++ models/class_contry.py
@@ -0,0 +1,31 @@
+from .city import City
+
+class Country:
+    """Defines a country"""
+    
+    def __init__(self, name):
+        """Initialize with a name"""
+        self.name = name
+        self.cities = []
+        self.__cities = []
+
+    def add_city(self, city):
+        """Add a city"""
+        if not isinstance(city, City):
+            raise TypeError("The city must be an instance of the City class")
+        self.cities.append(city)
+
+    @property
+    def name(self):
+        """Get the name"""
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        """Set the name"""
+        if not isinstance(value, str):
+            raise ValueError("The name must be a string")
+        if not value.strip():
+            raise ValueError("The name cannot be blank")
+        self.__name = value
+
