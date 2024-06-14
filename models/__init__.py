f36e3908d8164e758ebf3a991dc0927e04bac150
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-13 22:20:47 -0400

    deleted __init__ file and Interface file

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .city import City
-from .class_contry import Country
-from .class_reviews import Review
-from .users import User
-from .amenity import Amenity
-from .places import Places
