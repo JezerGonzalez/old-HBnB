dba3e0c99c91103ba254e31329cc35eb7a26e2c4
JezerGonzalez <jezergonzalez@gmail.com>
2024-06-14 15:32:59 -0400

    finished amenity end point

--- API/amenity_ep.py
+++ API/amenity_ep.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, abort
 from models.amenity import Amenity
+from persistence.DataManager import DataManager
 
 amenity_bp = Blueprint("amenity", __name__)
 
@@ -12,24 +13,25 @@ def create_amenity():
         abort(400, description="No data provided (must be JSON)")
     if "name" not in data:
         abort(400, description="Missing name")
-    amenity = Amenity.create(data["name"])
+    amenity = Amenity(data["name"])
+    amenity.save(amenity, "Amenity")
     return jsonify(amenity.to_dict()), 201
 
 
 @amenity_bp.route("/amenities", methods=["GET"])    
 def get_amenities():
     """Get all amenities"""
-    amenities = Amenity.all()
+    amenities = Amenity.all_entities("Amenity")
     if amenities is None:
         abort(404, description="No amenities found")
-    data = [amenity.to_dict() for amenity in amenities]
+    data = [amenity for amenity in amenities]
     return jsonify(data), 200
 
 
 @amenity_bp.route("/amenities/<amenity_id>", methods=["GET"])
 def get_amenity(amenity_id):
     """Get a specific amenity"""
-    amenity = Amenity.get(amenity_id)
+    amenity = Amenity.get(amenity_id, "Amenity")
     if amenity is None:
         abort(404, description="Amenity not found")
     return jsonify(amenity.to_dict()), 200
@@ -38,17 +40,17 @@ def get_amenity(amenity_id):
 @amenity_bp.route("/amenities/<amenity_id>", methods=["DELETE"])
 def delete_amenity(amenity_id):
     """Delete a specific amenity"""
-    amenity = Amenity.get(amenity_id)
+    amenity = Amenity.get(amenity_id, "Amenity")
     if amenity is None:
         abort(404, description="Amenity not found")
-    amenity.delete()
-    return jsonify(amenity.delete()), 200
+    DataManager().delete(amenity_id, "Amenity")
+    return jsonify("Amenity deleted successfully"), 201
 
 
 @amenity_bp.route("/amenities/<amenity_id>", methods=["PUT"])
 def update_amenity(amenity_id):
     """Update a specific amenity"""
-    amenity = Amenity.get(amenity_id)
+    amenity = Amenity.get(amenity_id, "Amenity")
     if amenity is None:
         abort(404, description="Amenity not found")
     data = request.json
